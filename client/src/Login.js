83d1fc8d6879fc83e46d5225c9d356143e0a917f
hoang thinhh <hoangthinhh.vn44@gmail.com>
2021-06-02 16:11:37 +0700

    push

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Container} from 'react-bootstrap'
-const AUTH_URL = " https://accounts.spotify.com/authorize?client_id=2674b2672eaa4125bc9cbfeea42b45e1&response_type=code&redirect_uri=http://vercel.com/nht-spec/spotify-clone&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = " https://accounts.spotify.com/authorize?client_id=2674b2672eaa4125bc9cbfeea42b45e1&response_type=code&redirect_uri=http://spotify-clone-blue.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 export default function Login() {
     return <Container>
         <a className="btn btn-success btn-lg" href={AUTH_URL}>
